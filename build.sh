1873d136d7a70ad63530b121ac4e2a9740472a87
Dusan Radovanovic <dushanr@gmail.com>
2017-01-10 00:17:11 -0800

    Added livereload to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 concurrently --prefix "name" \
-    --names "cine_list,cine_movi,imdb_movi" \
+    --names "cine_list,cine_movi,imdb_movi,livereloa" \
+    --prefix-colors "green,green,green,gray" \
     "tsc -w --pretty -p src/cineplex_list/tsconfig.json" \
     "tsc -w --pretty -p src/cineplex_movie/tsconfig.json" \
-    "tsc -w --pretty -p src/imdb_movie/tsconfig.json"
+    "tsc -w --pretty -p src/imdb_movie/tsconfig.json" \
+    "livereload"
--- release/cineplex_movie.js
+++ release/cineplex_movie.js
@@ -1,30 +1,3 @@
-function processOneTitle() {
-    var ratings = [
-        '<div class="ratings">',
-        '<div class="rating imdb">',
-        '<div class="icon">&nbsp;</div>',
-        '<div class="title">IMDB score:&nbsp;</div>',
-        '<div class="score"></div>',
-        '</div>',
-        '<div class="rating tomatometer">',
-        '<div class="icon">&nbsp;</div>',
-        '<div class="title">Tomatometer:&nbsp;</div>',
-        '<div class="score"></div>',
-        '</div>',
-        '</div>'
-    ].join('');
-    var movieHeader = $('.movie-header');
-    var movieName = movieHeader.find('.movie-title').text();
-    movieHeader.find('.movie-title').after(ratings);
-    getMovieLinks(movieName, function (err, r) {
-        movieHeader.find('.ratings .imdb .score').html(r.imdbHTML);
-        movieHeader.find('.ratings .tomatometer .score').html(r.tomatoHTML);
-    });
-}
-function main() {
-    processOneTitle();
-}
-main();
 function getMovieLinks(movieName, cb) {
     movieName = movieName.split('- An IMAX 3D Experience')[0].trim();
     movieName = movieName.split('– An IMAX 3D Experience')[0].trim();
